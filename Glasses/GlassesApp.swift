ccfd7cc387b5932f33910121b49db67183bc3080
Luca Langella <langellaluca00@gmail.com>
2026-02-25 15:19:23 +0100

    added assets and ar debugger

---
+++ Glasses/GlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassesApp.swift
+//  Glasses
+//
+//  Created by Luca Langella 1 on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct GlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OptometryFormView()
+        }
+    }
+}
---
+++ Glasses/RxInputTable.swift
@@ -0,0 +1,18 @@
+//
+//  RxInputView.swift
+//  Glasses
+//
+//  Created by Luca Langella 1 on 2/25/26.
+//
+
+import SwiftUI
+
+struct RxInputTable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RxInputTable()
+}
